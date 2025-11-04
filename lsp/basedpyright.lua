6c69329f70220a4499607b1a31c17b956d341580
deparr <dparrott@protonmail.com>
2025-11-04 13:49:16 -0700

    lsps

---
+++ lsp/basedpyright.lua
@@ -0,0 +1,14 @@
+return {
+  cmd = { "basedpyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json" },
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+}
--- lsp/tsgo.lua
+++ lsp/tsgo.lua
@@ -1,14 +1,20 @@
 return {
-  cmd = { "bun", "x", "--bun", "vtsls", "--stdio" },
+  cmd = { "bun", "tsgo", "--lsp", "-stdio" },
+
   filetypes = {
+
     "javascript",
+
     "javascriptreact",
+
     "javascript.jsx",
+
     "typescript",
+
     "typescriptreact",
+
     "typescript.tsx",
   },
-  root_markers = {
-    "tsconfig.json", "package.json", "jsconfig.json",
-  },
+
+  root_markers = { "tsconfig.json", "jsconfig.json", "package.json" },
 }
