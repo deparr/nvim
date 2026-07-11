19df87ca7641a1d99b86709813ef7d1fc2bab1bf
deparr <dparrott@protonmail.com>
2026-07-10 22:22:35 -0600

    nvim changed lua option handling
     https://github.com/neovim/neovim/pull/39849

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -8,14 +8,29 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 local afgroup = vim.api.nvim_create_augroup("autoformatopts", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "BufRead" }, {
   callback = function()
-    vim.opt.formatoptions = vim.opt.formatoptions - "a" - "t" + "c" + "q" - "o" + "r" + "n" + "j" - "2"
+    -- #39849 lua option handling was cleaned up but now doesnt support chained infix ops
+    vim.opt.formatoptions = vim.opt.formatoptions - "a"
+    vim.opt.formatoptions = vim.opt.formatoptions - "t"
+    vim.opt.formatoptions = vim.opt.formatoptions + "c"
+    vim.opt.formatoptions = vim.opt.formatoptions + "q"
+    vim.opt.formatoptions = vim.opt.formatoptions - "o"
+    vim.opt.formatoptions = vim.opt.formatoptions + "r"
+    vim.opt.formatoptions = vim.opt.formatoptions + "n"
+    vim.opt.formatoptions = vim.opt.formatoptions + "j"
+    vim.opt.formatoptions = vim.opt.formatoptions - "2"
   end,
   group = afgroup,
 })
 
 local text = vim.api.nvim_create_augroup("text", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "text", "markdown", "mail", "gitcommit", "djot" },
+  pattern = { "text", "markdown", "mail", "djot" },
   group = text,
-  command = "setlocal spell tw=72 colorcolumn=73",
+  command = "setlocal spell textwidth=72",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit" },
+  group = text,
+  command = "setlocal spell textwidth=50"
 })
